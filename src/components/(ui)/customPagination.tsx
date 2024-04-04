c55a9028d1e36c5043bac41c333c79db17d81ee6
quanber123 <quantm103@gmail.com>
2024-04-04 15:06:56 +0700

    [fix] custom Pagination

--- src/components/(ui)/customPagination.tsx
+++ src/components/(ui)/customPagination.tsx
@@ -1,129 +1,112 @@
 import React, { useCallback } from 'react';
-import { PaginationItemType, usePagination } from '@nextui-org/react';
+import {
+  Pagination,
+  PaginationItemRenderProps,
+  PaginationItemType,
+} from '@nextui-org/react';
 import useQueryString from '@/lib/hooks/useQueryString';
 import { useSearchParams } from 'next/navigation';
 import scrollElement from '@/lib/utils/scrollElement';
+
 type Props = {
   totalPage: number;
   isScroll?: boolean;
 };
+
 const CustomPagination: React.FC<Props> = ({ totalPage, isScroll }) => {
   const [createQueryString] = useQueryString();
   const searchQuery = useSearchParams();
-  const curPage = searchQuery.get('page') || 1;
-  const { activePage, range, setPage, onNext, onPrevious } = usePagination({
-    total: totalPage,
-    showControls: true,
-    siblings: 6,
-    boundaries: 10,
-  });
-
+  const curPage = Number(searchQuery.get('page')) || 1;
   const handleNext = useCallback(
     (page: string) => {
-      const currPage = Number(curPage);
       if (page === 'next') {
-        onNext();
-        setPage(activePage + 1);
-        const nextPage = currPage >= totalPage ? totalPage : currPage + 1;
+        const nextPage = curPage >= totalPage ? totalPage : curPage + 1;
         createQueryString('page', nextPage.toString());
         isScroll && scrollElement();
       }
     },
-    [
-      createQueryString,
-      activePage,
-      onNext,
-      searchQuery,
-      setPage,
-      totalPage,
-      scrollElement,
-      isScroll,
-    ]
+    [createQueryString, searchQuery, totalPage, scrollElement, isScroll]
   );
 
   const handlePrevious = useCallback(
     (page: string) => {
-      const currPage = Number(curPage);
       if (page === 'prev') {
-        onPrevious();
-        setPage(activePage - 1);
-        const prev = currPage <= 1 ? 1 : currPage - 1;
+        const prev = curPage <= 1 ? 1 : curPage - 1;
         createQueryString('page', prev.toString());
         isScroll && scrollElement();
       }
     },
-    [
-      createQueryString,
-      activePage,
-      onPrevious,
-      setPage,
-      searchQuery,
-      scrollElement,
-      isScroll,
-    ]
+    [createQueryString, searchQuery, scrollElement, isScroll]
   );
   const handleSetPage = useCallback(
     (page: number) => {
-      setPage(page);
       createQueryString('page', page.toString());
       isScroll && scrollElement();
     },
-    [createQueryString, setPage, scrollElement, isScroll]
+    [createQueryString, scrollElement, isScroll]
   );
+
+  const renderItems = ({ key, value }: PaginationItemRenderProps) => {
+    if (value === PaginationItemType.NEXT) {
+      return (
+        <button
+          key={key}
+          className={`w-max h-full px-2 py-1 rounded border border-gray-200 text-gray-500 ${
+            curPage >= totalPage && 'cursor-not-allowed'
+          }`}
+          onClick={() => handleNext(value)}
+          disabled={curPage >= totalPage}
+        >
+          Next
+        </button>
+      );
+    }
+
+    if (value === PaginationItemType.PREV) {
+      return (
+        <button
+          key={key}
+          className={`w-max h-full px-2 py-1 rounded border border-gray-200 text-gray-500 ${
+            curPage <= 1 && 'cursor-not-allowed'
+          }`}
+          onClick={() => handlePrevious(value)}
+          disabled={curPage <= 1}
+        >
+          Prev
+        </button>
+      );
+    }
+
+    if (value === PaginationItemType.DOTS) {
+      return (
+        <button key={key} className='rounded-md px-2 py-1'>
+          ...
+        </button>
+      );
+    }
+
+    return (
+      <button
+        key={key}
+        className={`w-[32px] h-full px-2 py-1 border border-gray-200 rounded
+          ${curPage === value && 'bg-secondary text-gray-100'}`}
+        onClick={() => handleSetPage(value)}
+      >
+        {value}
+      </button>
+    );
+  };
+
   return (
-    <div className='flex flex-col gap-2'>
-      <ul className='flex gap-2 items-center'>
-        {range.map((page) => {
-          if (page === PaginationItemType.NEXT) {
-            return (
-              <li key={page}>
-                <button
-                  className={`w-max h-full px-2 py-1 rounded border border-gray-200 text-gray-500 ${
-                    Number(curPage) >= totalPage && 'cursor-not-allowed'
-                  }`}
-                  onClick={() => handleNext(page)}
-                  disabled={Number(curPage) >= totalPage}
-                >
-                  Next
-                </button>
-              </li>
-            );
-          }
-
-          if (page === PaginationItemType.PREV) {
-            return (
-              <li key={page}>
-                <button
-                  className={`w-max h-full px-2 py-1 rounded border border-gray-200 text-gray-500 ${
-                    Number(curPage) <= 1 && 'cursor-not-allowed'
-                  }`}
-                  onClick={() => handlePrevious(page)}
-                  disabled={Number(curPage) <= 1}
-                >
-                  Prev
-                </button>
-              </li>
-            );
-          }
-
-          if (page === PaginationItemType.DOTS) {
-            return <li key={page}>...</li>;
-          }
-
-          return (
-            <li key={page}>
-              <button
-                className={`w-[32px] h-full px-2 py-1 border border-gray-200 rounded
-                 ${Number(curPage) === page && 'bg-secondary text-gray-100'}`}
-                onClick={() => handleSetPage(page)}
-              >
-                {page}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <Pagination
+      showControls
+      color='secondary'
+      page={curPage}
+      total={totalPage}
+      siblings={1}
+      boundaries={1}
+      renderItem={renderItems}
+    />
   );
 };
 
