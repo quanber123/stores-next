1f33749b8e66e75551461896de86c524f0fa3b14
quanber123 <quantm103@gmail.com>
2024-04-08 00:32:14 +0700

    [style] loading item

--- src/components/(ui)/loadingItem.tsx
+++ src/components/(ui)/loadingItem.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 type Props = {
   sectionClass?: string;
+  articleClass?: string;
   heightItem?: string;
   amount: number;
 };
-const LoadingItem: React.FC<Props> = ({ sectionClass, heightItem, amount }) => {
+const LoadingItem: React.FC<Props> = ({
+  sectionClass,
+  articleClass,
+  heightItem,
+  amount,
+}) => {
   const loadingItems = [];
   for (let i = 0; i < amount; i++) {
     loadingItems.push(
-      <article className={`flex flex-col gap-[24px] ${heightItem}`} key={i}>
+      <article
+        className={`flex flex-col gap-[24px] ${heightItem} ${articleClass}`}
+        key={i}
+      >
         <div className='skeleton w-full h-3/4'></div>
         <div className='w-full flex flex-col gap-[5px]'>
           <div className='skeleton w-full h-[24px]'></div>
