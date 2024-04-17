9a8d72f4b6ac26270db38ef07a457b15e82bcdef
quanber123 <quantm103@gmail.com>
2024-04-17 18:32:48 +0700

    [feat] optimize image + fix layout shop

--- src/app/shop/[id]/layout.tsx
+++ src/app/shop/[id]/layout.tsx
@@ -1,35 +1,10 @@
-'use client';
-import { useGetProductsByIdQuery } from '@/lib/redux/query/productQuery';
-import { redirect, useParams } from 'next/navigation';
 import React from 'react';
 
 interface RootLayoutProps {
   children: React.ReactNode;
-  images: React.ReactNode;
-  products: React.ReactNode;
-  information: React.ReactNode;
 }
 export default function ProductDetailsLayout({
   children,
-  images,
-  information,
-  products,
 }: RootLayoutProps): JSX.Element {
-  const { id } = useParams();
-  const {
-    data: productData,
-    isSuccess: isSuccessProduct,
-    isError: isErrorProduct,
-  } = useGetProductsByIdQuery(id);
-  if (isErrorProduct) {
-    redirect(`/not-found-product-${id}`);
-  }
-  return (
-    <>
-      {children}
-      {images}
-      {information}
-      {products}
-    </>
-  );
+  return <>{children}</>;
 }
